3898b8c9ff2f3fd0edf28658cc0de774d5251f15
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-03-02 20:00:34 -0300

    change city-api

--- datasources/city-api.js
+++ datasources/city-api.js
@@ -1,13 +1,35 @@
 const { RESTDataSource } = require('apollo-datasource-rest');
+const axios = require('axios');
 
 class CityAPI extends RESTDataSource {
     constructor() {
         super();
-        this.baseURL = 'https://localhost:44352/';
+        this.baseURL = 'http://localhost:5000/';
     }
 
-    getAll() {
-        this.get('city');
+    async getAll(token) {
+        const result = await axios.get(this.baseURL + 'city', { headers: { 'Authorization': 'Bearer ' + token } });
+        return result.data;
+    }
+
+    async get(token, id) {
+        const result = await axios.get(this.baseURL + `city${id}`, { headers: { 'Authorization': 'Bearer ' + token } });
+        return result.data;
+    }
+
+    async post(token, city) {
+        const result = await axios.post(this.baseURL + 'city', city, { headers: { 'Authorization': 'Bearer ' + token } });
+        return result.data;
+    }
+
+    async put(token, id, city) {
+        const result = await axios.get(this.baseURL + `city${id}`, city, { headers: { 'Authorization': 'Bearer ' + token } });
+        return result.data;
+    }
+
+    async delete(token, id) {
+        const result = await axios.get(this.baseURL + `city${id}`, { headers: { 'Authorization': 'Bearer ' + token } });
+        return result.data;
     }
 }
 
