a639192517f11f201ee16489fe018860cb30333e
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2023-03-11 16:59:51 -0300

    Changes on city and customer's schema

--- datasources/customer-api.js
+++ datasources/customer-api.js
@@ -7,6 +7,54 @@ class CustomerAPI extends BasicAPI {
     constructor(token) {
         super(token, 'https://localhost:5001/customer');
     }
+
+    async getPhones(customerId) {
+        try {
+            return (await axios.get(`${this.url}/${customerId}/phone`, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
+
+    async postPhone(customerId, phone) {
+        try {
+            return (await axios.post(`${this.url}/${customerId}/phone`, phone, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
+
+    async putPhone(customerId, phoneId, phone) {
+        try {
+            return (await axios.put(`${this.url}/${customerId}/phone/${phoneId}`, phone, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
+
+    async getAddresses(customerId) {
+        try {
+            return (await axios.get(`${this.url}/${customerId}/address`, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
+
+    async postAddresses(customerId, address) {
+        try {
+            return (await axios.post(`${this.url}/${customerId}/address`, address, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
+
+    async putAddresses(customerId, addressId, address) {
+        try {
+            return (await axios.put(`${this.url}/${customerId}/address/${addressId}`, address, this.header())).data;
+        } catch (error) {
+            throw new Error(this.errorMessage(error));
+        }
+    }
 }
 
 module.exports = CustomerAPI;
--- resolvers.js
+++ resolvers.js
@@ -7,28 +7,57 @@ const resolvers = {
         city: (_, { id }, { dataSources }) => {
             return dataSources.cityAPI.get(id);
         },
+
         //customer
         customers: (_, __, { dataSources }) => {
             return dataSources.customerAPI.getAll();
         },
         customer: (_, { id }, { dataSources }) => {
             return dataSources.customerAPI.get(id);
-        },
+        }
     },
     Mutation: {
         //city
         createCity: async (_, { input }, { dataSources }) => {
             return await dataSources.cityAPI.post(input);
         },
-        updateCity: async (_, { id, version, input }, { dataSources }) => {
-            return await dataSources.cityAPI.put(id, version, input);
+        updateCity: async (_, { input }, { dataSources }) => {
+            return await dataSources.cityAPI.put(input.id, input.version, input);
         },
         deleteCity: async (_, { id }, { dataSources }) => {
             return await dataSources.cityAPI.delete(id);
         },
+
         //customer
         createCustomer: async (_, { input }, { dataSources }) => {
-            return await dataSources.customerAPI.post(input);
+            const customer = await dataSources.customerAPI.post(input);
+            if (input.addresses) {
+                input.addresses.forEach(async address => {
+                    customer.addresses.push(await dataSources.customerAPI.postAddress(address));
+                });
+            }
+            if (input.phones) {
+                input.phones.forEach(async phone => {
+                    customer.addresses.push(await dataSources.customerAPI.postPhone(phone));
+                });
+            }
+            return customer;
+        },
+        deleteCustomer: async (_, { id }, { dataSources }) => {
+            return await dataSources.customerAPI.delete(id);
+        },
+    },
+    Customer: {
+        phones: async (customer, _, { dataSources }) => {
+            return dataSources.customerAPI.getPhones(customer.id);
+        },
+        addresses: async (customer, _, { dataSources }) => {
+            return dataSources.customerAPI.getAddresses(customer.id);
+        }
+    },
+    Address: {
+        city: async (address, _, { dataSources }) => {
+            return dataSources.cityAPI.get(address.cityId)
         }
     }
 }
